09deee8e8acc14f365fd0b979ff3c4e93025b72a
Hanif-Niomic <m.alihanif249@gmail.com>
2022-04-04 21:44:01 +0700

    tugas java
    tugas java

---
+++ tugas19.js
@@ -0,0 +1,21 @@
+function tugas19a(){
+  var kata = "Semangat...";
+  console.log(kata.repeat(5));
+}
+
+function tugas19b(){
+  var bintang = "*";
+  console.log(bintang.repeat(5));
+  console.log(bintang.repeat(25));
+  console.log(bintang.repeat(45));
+  console.log(bintang.repeat(65));
+  console.log(bintang.repeat(85));
+  console.log(bintang.repeat(65));
+  console.log(bintang.repeat(45));
+  console.log(bintang.repeat(25));
+  console.log(bintang.repeat(5));
+}
+
+
+tugas19a()
+tugas19b()
